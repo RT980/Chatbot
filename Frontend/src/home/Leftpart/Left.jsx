03abdcedb8698264d7c4a749831a3b69e007c6ad
RT980 <87111723+RT980@users.noreply.github.com>
2025-05-13 17:40:00 +0545

    add new

--- Frontend/src/home/Leftpart/Left.jsx
+++ Frontend/src/home/Leftpart/Left.jsx
@@ -3,14 +3,12 @@ import Search from "./Search";
 import Users from "./Users";
 import Logout from "./Logout";
 
+
 function Left() {
   return (
-    <div className="w-full   bg-black text-gray-300">
+    <div className="w-full bg-black text-gray-300">
       <Search />
-      <div
-        className=" flex-1  overflow-y-auto"
-        style={{ minHeight: "calc(84vh - 10vh)" }}
-      >
+      <div className="flex-1 overflow-y-auto min-h-[calc(84vh-10vh)]">
         <Users />
       </div>
       <Logout />
